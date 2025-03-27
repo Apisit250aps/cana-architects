e69c16f5b4fd7b490d7679edff22be6f7d98b173
YUapisit <aps.apisit250@gmail.com>
2025-03-27 20:57:03 +0700

    protocol

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "canaarchitects.com",
         pathname: "/uploads/**",
       },
